7d7e301e681f7f380508c626fa3fc4fd4e803a36
Zachary Stewart <zstewart@google.com>
2020-12-21 09:59:39 -0500

    Run `cargo fmt` to make file formatting consistent

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate clap;
 
-use clap::{Arg, App};
+use clap::{App, Arg};
 use std::fs::File;
 
 mod memdev;
@@ -13,26 +13,31 @@ fn main() {
     let argparser = App::new("FEO3Boy")
         .version("0.1.0")
         .author("Ciaran Cain <ciaran2@umbc.edu>")
-        .arg(Arg::with_name("bios")
-                 .short("b")
-                 .long("bios")
-                 .takes_value(true)
-                 .help("File containing BIOS dump"))
-        .arg(Arg::with_name("rom")
-                 .short("r")
-                 .long("rom")
-                 .takes_value(true)
-                 .help("File containing ROM dump"))
+        .arg(
+            Arg::with_name("bios")
+                .short("b")
+                .long("bios")
+                .takes_value(true)
+                .help("File containing BIOS dump"),
+        )
+        .arg(
+            Arg::with_name("rom")
+                .short("r")
+                .long("rom")
+                .takes_value(true)
+                .help("File containing ROM dump"),
+        )
         .get_matches();
 
     let mut mmu = memdev::GbMmu::new();
     let mut cpustate = gbz80core::Gbz80state::new();
 
     match argparser.value_of("bios") {
-      Some(filename) => { let mut bios_file = File::open(filename).unwrap();
-                          mmu.set_bios(memdev::BiosRom::new(&mut bios_file));
-                        },
-      None => (),
+        Some(filename) => {
+            let mut bios_file = File::open(filename).unwrap();
+            mmu.set_bios(memdev::BiosRom::new(&mut bios_file));
+        }
+        None => (),
     }
 
     loop {
