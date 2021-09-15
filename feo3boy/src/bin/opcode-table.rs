ff7b2c613fa8cd0846af2e33f8c1f6f11beaf21b
Zachary Stewart <zstewart@google.com>
2021-09-15 16:09:35 -0400

    add a utility binary that prints the decoded opcodes as a CSV table for comparison against published opcode tables

---
+++ feo3boy/src/bin/opcode-table.rs
@@ -0,0 +1,18 @@
+//! Prints out all opcodes in a CSV table for comparison against published opcode tables.
+
+use feo3boy::gbz80core::opcode::Opcode;
+
+fn main() {
+    for l in 0u8..=0xF {
+        print!(",{:#04X}", l);
+    }
+    println!();
+    for h in (0u8..=0xFF).step_by(0x10) {
+        print!("{:#04X}", h);
+        for l in 0u8..=0xF {
+            let opcode = Opcode::decode(h + l);
+            print!(",\"{}\"", opcode);
+        }
+        println!();
+    }
+}
